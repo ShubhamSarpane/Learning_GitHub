f28c043d009a527c21aa5d655598becac6c2ff9e
ShubhamSarpane <sarpaneshubhams97@gmail.com>
2024-01-13 17:05:25 +0530

    Added try 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Try");
         System.out.println("Try2");
+        System.out.println("Try3");
     }
 }
